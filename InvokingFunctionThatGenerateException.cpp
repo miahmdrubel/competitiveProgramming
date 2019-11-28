0e3ab531182615cdc70eee4de0b22a5caa2e7502
miahmdrubel <miahmdrubel384@gmail.com>
2019-11-28 20:06:32 +0600

    Add files via upload

---
+++ InvokingFunctionThatGenerateException.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void test(int x)
+{
+    if(x==0) throw 1;
+    if(x==1) throw 1.0;
+    //throw 'x';
+}
+
+
+
+int main()
+{
+    int m;
+    cin >> m;
+
+    try{
+       test(m);
+       cout<<"Exiting try."<<endl;
+
+     }
+
+       catch(...)
+       {
+           cout << "Catch all."<< endl;
+       }
+
+      cout <<"Finish the Code!!!!" << endl;
+
+     return 0;
+}
