4e8240781cc1f76c1ba9bea8c8e2dc61c6858d5d
miahmdrubel <miahmdrubel384@gmail.com>
2020-01-02 22:00:40 +0600

    Add files via upload
    UVa Online Judge problem
    12577 - Hajj-e-Akbar

---
+++ Hajj-e-akbar.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    string h = "hajj";
+    string u = "Umrah";
+    string i;
+
+
+    while(i!="*"){
+
+    cin >> i;
+
+    if(i==h){
+        cout<<" Hajj-e-Akbar\n";
+    }
+   if(i==u) {
+     cout<<"Hajj-e-Akshar\n";
+    }
+
+
+
+    }
+
+
+    return 0;
+}
